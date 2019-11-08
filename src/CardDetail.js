fe4c2b8cc4db0851efdcefb1257995bffe6d92b7
BreezyP <jorge.a.garcia12@gmail.com>
2019-11-08 10:01:42 -0600

    my first react component

---
+++ src/CardDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const CardDetail = (props) => {
+    return (
+        <div>
+            <Card>
+                <CardImg top width="100%" height="310 px" style={{objectFit: 'cover'}} src="https://images.unsplash.com/photo-1573158612982-034c1494d647?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="Card image cap" />                <CardBody>
+                <CardTitle> <h1>{props.name}</h1></CardTitle>
+                    <CardSubtitle>{props.job}</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
+};
+
+export default CardDetail;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import CardDetail from "./CardDetail";
+
+const App = () => {
+    return (
+        <div className='row justify-content-center mt-2'>
+            <div className="col-3">
+                <CardDetail name='Samantha' job='My Companion'/>
+            </div>
+            <div className="col-3">
+                <CardDetail name='Lillian' job="My Daughter"/>
+            </div>
+            <div className="col-3">
+                <CardDetail name='Maximillian' job="My Future Son"/>
+            </div>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
